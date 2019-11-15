fcbb7e45a32c2fdea3044b1c5ad46c2e114a71cf
gokul921998 <46988010+gokul921998@users.noreply.github.com>
2019-11-15 12:56:20 +0530

    First Commit

---
+++ Final_Check/src/com/cognizant/moviecruiser/dao/MovieDaoCollectionImpl.java
@@ -0,0 +1,138 @@
+/**
+ * 
+ */
+package com.cognizant.moviecruiser.dao;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.cognizant.moviecruiser.model.Movie;
+import com.cognizant.moviecruiser.util.DateUtil;
+
+/**
+ * @Gokul
+ *
+ */
+public class MovieDaoCollectionImpl implements MovieDao {
+
+	private List<Movie> movieList;
+
+	/**
+	 * 
+	 */
+	public MovieDaoCollectionImpl() {
+
+		if (movieList == null) {
+			movieList = new ArrayList<Movie>();
+			try {
+				movieList.add(new Movie(1, "Avatar", 2787965087f, true,
+						DateUtil.convertToDate("15/03/2017"), "ScienceFiction",
+						true));
+
+				movieList.add(new Movie(2, "The Avengers", 1518812988f, true,
+						DateUtil.convertToDate("23/12/2017"), "Superhero",
+						false));
+
+				movieList
+						.add(new Movie(3, "Titanic", 21874639444f, true, DateUtil
+								.convertToDate("07/02/2017"), "Romance", false));
+
+				movieList.add(new Movie(4, "Hugo", 1671713208f, false,
+						DateUtil.convertToDate("09/02/2017"), "ScienceFiction",
+						true));
+
+				movieList.add(new Movie(5, "Civil War", 2750760348f,
+						true, DateUtil.convertToDate("25/01/2022"),
+						"Superhero", true));
+			} catch (ParseException e) {
+				System.out.println("Parse Exception: " + e.getMessage());
+			}
+		}
+	}
+
+	/**
+	 * @return the movieList
+	 */
+	public List<Movie> getMovieList() {
+		return movieList;
+	}
+
+	/**
+	 * @param movieList
+	 *            the movieList to set
+	 */
+	public void setMovieList(List<Movie> movieList) {
+		this.movieList = movieList;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.cognizant.moviecruiser.dao.MovieDao#getMovieListAdmin()
+	 */
+	@Override
+	public List<Movie> getMovieListAdmin() {
+		// TODO Auto-generated method stub
+		return movieList;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.cognizant.moviecruiser.dao.MovieDao#getMovieListCustomer()
+	 */
+	@Override
+	public List<Movie> getMovieListCustomer() {
+		// TODO Auto-generated method stub
+		List<Movie> movieListCustomer = new ArrayList<Movie>();
+		Date today = new Date();
+		for (Movie movie : movieList) {
+			if (movie.getDateOfLaunch().getTime() <= today.getTime()
+					&& movie.isActive()) {
+				movieListCustomer.add(movie);
+			}
+		}
+		return movieListCustomer;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.cognizant.moviecruiser.dao.MovieDao#modifyMovie(com.cognizant.
+	 * moviecruiser.model.Movie)
+	 */
+	@Override
+	public void modifyMovie(Movie movie) {
+
+		for (Movie movieForModification : movieList) {
+			if (movie.getId() == movieForModification.getId()) {
+				movieForModification.setTitle(movie.getTitle());
+				movieForModification.setGenre(movie.getGenre());
+				movieForModification.setDateOfLaunch(movie.getDateOfLaunch());
+				movieForModification.setHasTeaser(movie.isHasTeaser());
+				movieForModification.setBoxOffice(movie.getBoxOffice());
+				movieForModification.setActive(movie.isActive());
+			}
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.cognizant.moviecruiser.dao.MovieDao#getMovie(java.lang.Long)
+	 */
+	@Override
+	public Movie getMovie(Long movieId) {
+
+		for (Movie movieForFetching : movieList) {
+			if (movieId == movieForFetching.getId()) {
+				return movieForFetching;
+			}
+		}
+		return null;
+	}
+
+}
